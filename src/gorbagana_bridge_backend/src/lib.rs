8e4941d978737010d53c87479ea56f34d88be34a
Rachit2323 <atreyrachit23@gmail.com>
2025-09-03 14:55:03 +0530

    singer_working

--- src/gorbagana_bridge_backend/src/lib.rs
+++ src/gorbagana_bridge_backend/src/lib.rs
@@ -42,11 +42,61 @@ struct ManagementCanisterSignatureReply {
     pub signature: Vec<u8>,
 }
 
+#[ic_cdk::update]
+async fn generate_keypair_solana_user() -> Result<String, String> {
+
+    let request = ManagementCanisterSchnorrPublicKeyRequest {
+        canister_id: None,
+        derivation_path: vec![ic_cdk::api::caller().as_slice().to_vec()],
+        key_id: SchnorrKeyId {
+            algorithm: SchnorrAlgorithm::Ed25519,
+            name: String::from("dfx_test_key"),
+        },
+    };
+
+    ic_cdk::println!("generate_keypair_solana: {:?}" ,request);
+
+    let (res,): (ManagementCanisterSchnorrPublicKeyReply,) = ic_cdk::call(
+        Principal::management_canister(),
+        "schnorr_public_key",
+        (request,),
+    )
+    .await // Add the await keyword
+    .map_err(|e| format!("schnorr_public_key failed {}", e.1))?;
+
+    // Generate or obtain the private key
+
+    ic_cdk::println!("res {:?}", res);
+    let public_key_bytes = res.public_key.to_vec();
+
+
+    // ic_cdk::println!("public_key_bytes {:?}", public_key_bytes);
+    let hex_string: String = public_key_bytes.iter()
+    .map(|b| format!("{:02X}", b)) // Convert each byte to uppercase hex
+    .collect();
+
+    ic_cdk::println!("Raw Public Key (Hex): {}", hex_string);
+
+
+    if public_key_bytes.len() != 32 {
+        return Err("Invalid public key length; expected 32 bytes".to_string());
+    }
+
+    // Convert the public key to a Solana address (Base58 encoding)
+    // let solana_address = encode_base58(&public_key_bytes);
+    let solana_address = bs58::encode(public_key_bytes).into_string();
+    // let pubkey = Pubkey::new(&public_key_bytes);
+    ic_cdk::println!("Solana Address: {}", solana_address);
+
+    Ok(solana_address)
+}
+
+
 
 #[ic_cdk::update]
 async fn generate_keypair_solana() -> Result<String, String> {
 
-    let derivation_path = vec![vec![0, 0, 0, 0]];
+    let derivation_path = vec![vec![34, 93, 83, 88, 49, 84, 44, 44]];
 
     let request = ManagementCanisterSchnorrPublicKeyRequest {
         canister_id: None,
@@ -106,7 +156,8 @@ async fn sign_transaction_solana(hash: String) -> Result<String, String> {
     ic_cdk::println!("hash_bytes {:?}",hash_bytes);
     // Create a Sha256 hasher
 
-    let derivation_path = vec![vec![0, 0, 0, 0]];
+    let derivation_path = vec![vec![34, 93, 83, 88, 49, 84, 44, 44]];
+
 
     let internal_request = ManagementCanisterSignatureRequest {
         message: hash_bytes,
